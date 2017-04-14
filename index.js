38a64a4b1bc3f116b51535eae566c6aa7308b93e
Gilbert <steven.n.gilbert@gmail.com>
2017-04-14 10:30:29 -0400

    fix: Fixex exercise 8

--- index.js
+++ index.js
@@ -141,50 +141,27 @@ sortLastNamesAlphabetically(people, exercises[6]);
 // Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
 
-function sumInstances(list, exercise) {
+function sumTransportationMethods(list, exercise) {
   const ul = document.createElement('ul');
 
-  const numCars = list.reduce((sum, modeOfTransport) => {
-    if (modeOfTransport === 'car') {
-      return sum + 1;
+  const sums = list.reduce((allNames, name) => {
+    const names = allNames;
+    if (name in names) {
+      names[name] += 1;
+    } else {
+      names[name] = 1;
     }
-    return sum;
-  }, 0);
+    return names;
+  }, {});
 
-  const numTrucks = list.reduce((sum, modeOfTransport) => {
-    if (modeOfTransport === 'truck') {
-      return sum + 1;
-    }
-    return sum;
-  }, 0);
+  exercise.appendChild(ul);
 
-  const numBikes = list.reduce((sum, modeOfTransport) => {
-    if (modeOfTransport === 'bike') {
-      return sum + 1;
-    }
-    return sum;
-  }, 0);
+  for (const prop in sums) {
+    const li = document.createElement('li');
 
-  const numWalks = list.reduce((sum, modeOfTransport) => {
-    if (modeOfTransport === 'walk') {
-      return sum + 1;
-    }
-    return sum;
-  }, 0);
-  const numVans = list.reduce((sum, modeOfTransport) => {
-    if (modeOfTransport === 'van') {
-      return sum + 1;
-    }
-    return sum;
-  }, 0);
-
-  console.log(`There are ${numCars} cars.`);
-  console.log(`There are ${numTrucks} trucks.`);
-  console.log(`There are ${numBikes} bikes.`);
-  console.log(`There are ${numWalks} ways to walk.`);
-  console.log(`There are ${numVans} vans.`);
-  console.log(list.length);
-  console.log(numCars + numTrucks + numBikes + numWalks + numVans);
+    li.textContent = `There are ${sums[prop]} ${prop}s.`
+    ul.appendChild(li);
+  }
 }
 
-sumInstances(data, exercises[7]);
+sumTransportationMethods(data, exercises[7]);
